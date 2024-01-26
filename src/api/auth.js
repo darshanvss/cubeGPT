ef6278e4fe72bff43ecaca76f3f88dc3ce2e05bd
darshanvss <darshanvss77@gmail.com>
2024-01-26 10:38:35 +0530

    added OAuth

---
+++ src/api/auth.js
@@ -0,0 +1,47 @@
+import axios from 'axios'
+
+const OAuthConfig = {
+  // Replace these values with your OAuth provider's configuration
+  clientId: import.meta.env.VITE_OAUTH_CLIENT_ID,
+  clientSecret: import.meta.env.VITE_OAUTH_CLIENT_SECRET,
+  redirectUri: import.meta.env.VITE_OAUTH_REDIRECT_URI,
+  authorizationEndpoint: 'https://www.worldcubeassociation.org/oauth/authorize',
+  tokenEndpoint: 'https://www.worldcubeassociation.org/oauth/token'
+}
+
+export const authenticate = async () => {
+  try {
+    // Construct the authorization URL
+    const authorizationUrl = `${OAuthConfig.authorizationEndpoint}?client_id=${OAuthConfig.clientId}&redirect_uri=${OAuthConfig.redirectUri}&response_type=code`
+
+    // Redirect the user to the authorization URL
+    window.location.href = authorizationUrl
+  } catch (error) {
+    console.error('Authentication error:', error)
+  }
+}
+
+export const handleAuthenticationCallback = async (code) => {
+  try {
+    // Exchange the authorization code for an access token
+    const response = await axios.post(OAuthConfig.tokenEndpoint, {
+      client_id: OAuthConfig.clientId,
+      client_secret: OAuthConfig.clientSecret,
+      redirect_uri: OAuthConfig.redirectUri,
+      code,
+      grant_type: 'authorization_code'
+    })
+
+    const accessToken = response.data.access_token
+
+    // Use the access token as needed (e.g., store it in Vuex, make API requests)
+    if (accessToken) {
+      console.log('Access token:', accessToken)
+      // You may want to store the token in a global state (e.g., Vuex) or use it for API requests
+    } else {
+      console.error('Authentication failed')
+    }
+  } catch (error) {
+    console.error('Authentication callback error:', error)
+  }
+}
--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,7 +1,32 @@
+// LoginView.vue
 <template>
   <main>
-    <h1 class="">HomeView</h1>
+    <h1 class="">Login Page</h1>
+    <button type="button" class="btn btn-outline-primary" @click="signIn">Login</button>
   </main>
 </template>
 
-<script setup></script>
+<script>
+import { authenticate, handleAuthenticationCallback } from '@/api/auth'
+
+export default {
+  name: 'LoginView',
+  data() {
+    return {
+      userName: ''
+    }
+  },
+  mounted() {
+    // Check for the presence of the authorization code in the URL
+    const code = new URLSearchParams(window.location.search).get('code')
+    if (code) {
+      handleAuthenticationCallback(code)
+    }
+  },
+  methods: {
+    signIn() {
+      authenticate()
+    }
+  }
+}
+</script>
