7f2787a313798c416cd493371d8ccfc20956e738
darshanvss <darshanvss77@gmail.com>
2024-01-27 14:51:31 +0530

    updated prompt with 'Results' table description

--- src/api/gemini.js
+++ src/api/gemini.js
@@ -3,7 +3,12 @@ const genAI = new GoogleGenerativeAI(import.meta.env.VITE_GOOGLE_GEMINI_API_KEY)
 
 const model = genAI.getGenerativeModel({ model: 'gemini-pro' })
 
-const prompt = 'You are a Pirate, make a song in 4 lines.'
+const prompt = `You are an expert at converting English sentences into SQL Queries. The Database has
+a table named "Results" with the following columns: id,pos,personId,personName,countryId,competitionId,
+eventId,roundTypeId,formatId,value1,value2,value3,value4,value5,best,average,regionalSingleRecord,
+regionalAverageRecord,updated_at. 
+
+`
 
 // Function to generate content based on the prompt and message
 export const generateTest = async (message) => {
